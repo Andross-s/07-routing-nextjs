b3c5544f8691656f7b99c1c6daece801b31e456d
Andrij Skoropad <andross_s@ukr.net>
2026-04-08 19:12:09 +0300

    rename file

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -2,19 +2,19 @@ import NoteList from "@/components/NoteList/NoteList";
 import { redirect } from "next/navigation";
 
 interface Props {
-  params: Promise<{ tag?: string[] }>;
+  params: Promise<{ slug?: string[] }>;
 }
 
 export default async function FilterPage({ params }: Props) {
   const resolvedParams = await params;
-  if (!resolvedParams.tag || resolvedParams.tag.length === 0) {
+  if (!resolvedParams.slug || resolvedParams.slug.length === 0) {
     redirect("/notes/filter/all");
   }
-  const tag = resolvedParams.tag[0];
+  const slug = resolvedParams.slug[0];
 
   return (
     <div>
-      <NoteList tag={tag === "all" ? undefined : tag} />
+      <NoteList tag={slug === "all" ? undefined : slug} />
     </div>
   );
 }
